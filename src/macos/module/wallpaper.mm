e448d174dbbf2c88998b0f1b7c0c269d61de2800
Fándly Gergő <gergo@paymoapp.com>
2021-07-14 13:02:03 +0300

    fix(macos): If wallpaper is set from a directory return the first file for the directory

--- src/macos/module/wallpaper.mm
+++ src/macos/module/wallpaper.mm
@@ -7,7 +7,37 @@ std::string wallpaper::getWallpaperPath() {
 	NSScreen* mainScreen = [NSScreen mainScreen];
 
 	NSURL* wallpaperUrl = [workspace desktopImageURLForScreen:mainScreen];
-	NSString* wallpaperPath = wallpaperUrl.absoluteString;
+
+	NSNumber* isDirectory;
+	[wallpaperUrl getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:nil];
+
+	NSString* wallpaperPath = @"";
+	if([isDirectory boolValue]) {
+		NSFileManager* fileManager = [NSFileManager defaultManager];
+		NSArray<NSString*>* files = [fileManager contentsOfDirectoryAtPath:wallpaperUrl.path error:nil];
+		NSArray<NSString*>* supportedExtensions = [NSArray arrayWithObjects:@"jpg", @"jpeg", @"png", nil];
+		
+		for(NSString* file in files) {
+			NSString* extension = file.pathExtension;
+
+			bool isImage = false;
+			for(NSString* supportedExtension in supportedExtensions) {
+				if([extension caseInsensitiveCompare:supportedExtension] == NSOrderedSame) {
+					isImage = true;
+					break;
+				}
+			}
+
+			if(isImage) {
+				NSURL* completeUrl = [wallpaperUrl URLByAppendingPathComponent:file];
+				wallpaperPath = completeUrl.absoluteString;
+				break;
+			}
+		}
+	}
+	else {
+		wallpaperPath = wallpaperUrl.absoluteString;
+	}
 
 	const char* path = [wallpaperPath UTF8String];
 	std::string pathStr = path;
