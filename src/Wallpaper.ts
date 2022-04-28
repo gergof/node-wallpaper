68f939ebf547c10c3ce12f2889bb752dd4b6622b
Ran-j <ran-junior@hotmail.com>
2022-04-28 17:31:33 -0300

    feat(windows): set wallpaper

--- src/Wallpaper.ts
+++ src/Wallpaper.ts
@@ -1,5 +1,6 @@
 interface Wallpaper {
 	get(): Promise<string>;
+	set(path: string): Promise<void>;
 }
 
 export default Wallpaper;
--- src/win/module/wallpaper.cpp
+++ src/win/module/wallpaper.cpp
@@ -1,7 +1,8 @@
 #include <windows.h>
 #include "wallpaper.h"
 
-std::u16string wallpaper::getWallpaperPath() {
+std::u16string wallpaper::getWallpaperPath()
+{
 	wchar_t path[MAX_PATH];
 	SystemParametersInfoW(SPI_GETDESKWALLPAPER, MAX_PATH, path, 0);
 
@@ -11,12 +12,26 @@ std::u16string wallpaper::getWallpaperPath() {
 	return pathStr16;
 }
 
-Napi::String wallpaper::_getWallpaperPath(const Napi::CallbackInfo& info) {
+void wallpaper::setWallpaper(const Napi::CallbackInfo& info)
+{
+	Napi::Env env = info.Env();
+	std::string path = info[0].ToString().Utf8Value();
+
+	std::wstring pathStr = std::wstring(path.begin(), path.end());
+	wchar_t* pathW = new wchar_t[pathStr.length() + 1];
+	wcscpy_s(pathW, pathStr.length() + 1, pathStr.c_str());
+
+	SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, pathW, SPIF_UPDATEINIFILE);
+}
+
+Napi::String wallpaper::_getWallpaperPath(const Napi::CallbackInfo &info)
+{
 	return Napi::String::New(info.Env(), wallpaper::getWallpaperPath());
 }
 
-Napi::Object wallpaper::Init(Napi::Env env, Napi::Object exports) {
+Napi::Object wallpaper::Init(Napi::Env env, Napi::Object exports)
+{
 	exports.Set("getWallpaperPath", Napi::Function::New(env, wallpaper::_getWallpaperPath));
-
+	exports.Set("setWallpaper", Napi::Function::New(env, wallpaper::setWallpaper));
 	return exports;
 }
